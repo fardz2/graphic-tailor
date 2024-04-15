16163d0376b916a832996af9efd66a7ad99bc6ce
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-04-15 21:22:18 +0800

    udpate metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,24 @@ import "./globals.css";
 const inter = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  generator: "Graphic Tailor",
+  applicationName: "Graphic Tailor",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "Graphic Tailor",
+    "Graphic tailor",
+    "graphic tailor",
+    "umkm",
+    "bandung",
+    "umkm jasa jahit",
+    "umkm jasa jahit di bandung",
+  ],
+  authors: [
+    { name: "Muhammad Faridz" },
+    { name: "Muhammad Faridz", url: "fardz2,vercel.app" },
+  ],
+  creator: "Muhammad Faridz",
+  publisher: "Muhammad Faridz",
   title: "Graphic Tailor",
   description:
     "Graphic tailor merupakan sebuah umkm yang bergerak pada bidang jasa perjahitan didaerah bandung.",
