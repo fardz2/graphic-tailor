7b02e78f379cacb8272c082e1ca020aa94e9afff
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-05-20 21:35:12 +0700

    update icon tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/image/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/image/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/image/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/image/site.webmanifest"></link>
+      </head>
       <body className={`${poppins.className}  `}>
         <div className="overflow-x-hidden">{children}</div>
       </body>
