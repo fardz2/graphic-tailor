7e2e88958b16a0d40b177f29a0cabdfbe5f478d8
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-05-28 21:15:41 +0700

    final

--- app/componets/Product.tsx
+++ app/componets/Product.tsx
@@ -1,35 +1,32 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 import { LayoutGrid } from "./ui/layout-grid";
+import { Fade } from "./animate/Fade";
 
 export function LayoutGridDemo() {
   return (
     <section className=" md:py-20 py-10" id="product">
       <div className=" container mx-auto md:px-24  md:mt-0 mt-20 ">
-        <h1 className="text-center md:text-5xl text-4xl font-bold">
-          Produk Kami
-        </h1>
-        <center>
-          <p>Hasil produk yang telah kami buat</p>
-        </center>
-
-        <LayoutGrid cards={cards} />
+        <Fade initial={-10} animate={0} delay={0.4}>
+          <h1 className="text-center md:text-5xl text-4xl font-bold">
+            Produk Kami
+          </h1>
+        </Fade>
+        <Fade initial={-10} animate={0} delay={0.5}>
+          <center>
+            <p>Hasil produk yang telah kami buat</p>
+          </center>
+        </Fade>
+        <Fade initial={-10} animate={0} delay={0.6}>
+          <LayoutGrid cards={cards} />
+        </Fade>
       </div>
     </section>
   );
 }
 
 const SkeletonOne = () => {
-  return (
-    <div>
-      <p className="font-bold text-4xl text-white">House in the woods</p>
-      <p className="font-normal text-base text-white"></p>
-      <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
-        A serene and tranquil retreat, this house in the woods offers a peaceful
-        escape from the hustle and bustle of city life.
-      </p>
-    </div>
-  );
+  return <div></div>;
 };
 
 const SkeletonTwo = () => {
@@ -47,54 +44,54 @@ const cards = [
     id: 1,
     content: <SkeletonOne />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/1.jpg",
   },
   {
     id: 2,
     content: <SkeletonTwo />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/2.jpg",
   },
   {
     id: 3,
     content: <SkeletonThree />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/3.jpg",
   },
   {
     id: 4,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/4.jpg",
   },
   {
     id: 5,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/5.jpg",
   },
   {
     id: 6,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/6.jpg",
   },
   {
     id: 7,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/7.jpg",
   },
   {
     id: 8,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/8.jpg",
   },
   {
     id: 9,
     content: <SkeletonFour />,
     className: "",
-    thumbnail: "/assets/image/sew.jpg",
+    thumbnail: "/assets/image/9.jpg",
   },
 ];
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Main from "./componets/Main";
+import NavBar from "./componets/NavBar";
+import About from "./componets/About";
+import Service from "./componets/Service";
+import { LayoutGridDemo } from "./componets/Product";
+import Footer from "./componets/Footer";
+
+export default function Page() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <About />
+      <Service />
+      <LayoutGridDemo />
+      <Footer />
+    </>
+  );
+}
